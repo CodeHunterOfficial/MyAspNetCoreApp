fecdf0bc3fc99c42756a2acd0a68678b096bfe02
Арабов Муллошараф Курбонович <arabov.mk@finist-soft.ru>
2025-09-15 12:29:23 +0300

    feat: initial commit  ASP.NET Core WebAPI (net9.0) with test suite

---
+++ MyWebApi.Tests/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using Xunit;
+
+namespace MyWebApi.Tests
+{
+    public class UnitTest1 : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public UnitTest1(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetWeatherForecast_ReturnsSuccess()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/weatherforecast");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
